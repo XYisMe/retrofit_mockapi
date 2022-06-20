2594514d587322d630fd093fbb32068c58bf310b
XYisMe <juni5584@gmail.com>
2022-06-20 17:19:04 +0800

    history list adapter finish

--- app/src/main/java/com/example/qrcodescanwithretrofitvolleyandmockapi/HistoryListAdapter.java
+++ app/src/main/java/com/example/qrcodescanwithretrofitvolleyandmockapi/HistoryListAdapter.java
@@ -1,5 +1,6 @@
 package com.example.qrcodescanwithretrofitvolleyandmockapi;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -35,6 +36,19 @@ public class HistoryListAdapter extends ListAdapter<History, HistoryListAdapter.
             tv_date = itemView.findViewById(R.id.date_text);
             tv_time = itemView.findViewById(R.id.time_text);
         }
+
+        //bind into view holder
+        public void bind(String location, String date, String time){
+            tv_location.setText(location);
+            tv_date.setText(date);
+            tv_time.setText(time);
+        }
+
+        static HistoryViewHolder create(ViewGroup parent) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.history_item, parent, false);
+            return new HistoryViewHolder(view);
+        }
     }
 
     protected HistoryListAdapter(@NonNull AsyncDifferConfig<History> config) {
@@ -44,12 +58,30 @@ public class HistoryListAdapter extends ListAdapter<History, HistoryListAdapter.
     @NonNull
     @Override
     public HistoryListAdapter.HistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return HistoryViewHolder.create(parent);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HistoryListAdapter.HistoryViewHolder holder, int position) {
+        History currentHistory = getItem(position);
+        holder.bind(
+                currentHistory.getLocation(),
+                currentHistory.getDate(),
+                currentHistory.getTime());
+    }
 
+    static class HistoryDiff extends DiffUtil.ItemCallback<History> {
+        @Override
+        public boolean areItemsTheSame(@NonNull History oldItem, @NonNull History newItem) {
+            return oldItem == newItem;
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull History oldItem, @NonNull History newItem) {
+            return oldItem.getDate().equals(newItem.getDate()) &&
+                    oldItem.getTime().equals(newItem.getTime()) &&
+                    oldItem.getLocation().equals(newItem.getLocation());
+        }
     }
 
 //    public class HistoryViewHolder extends RecyclerView.ViewHolder {
